6b5631056ca55145a627c18e9e15c6d82fe4a0a7
nttthoi <nttthoi@gmail.com>
2019-03-10 10:16:34 +0700

    update db

--- helloWordPrj/settings.py
+++ helloWordPrj/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'helloWordPrj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.officematter'),
+        'NAME': os.path.join(BASE_DIR, 'db.QuanLyThuVien'),
     }
 }
 
--- officematter/admin.py
+++ officematter/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from officematter.models import WebPage,Topic
 # Register your models here.
+
+admin.site.register(WebPage)
+admin.site.register(Topic)
