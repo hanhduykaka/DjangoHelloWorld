dcd7f0ee394f010257cf8c9304d49174fe5f681c
nttthoi <nttthoi@gmail.com>
2019-03-10 10:59:40 +0700

    add something can get from model Django

--- officematter/views.py
+++ officematter/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
-from officematter.models import Topic,WebPage
+from officematter.models import Topic, WebPage
 
 # Create your views here.
 
+
 def index(request):
-    return render(request,"officematter/index.html")
+    return render(request, "officematter/index.html")
+
 
 def about(request):
-    return render(request,"officematter/about.html")
+    return render(request, "officematter/about.html")
+
 
 def topicPage(request):
     topiclist = Topic.objects.all().order_by("top_name")
-    return render(request,"officematter/topic.html")
+    return render(request, "officematter/topic.html",context= {'topic':topiclist})
