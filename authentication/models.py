a0c8d44f75fa5dcf37b220bd2e73f8a48a42cea6
nttthoi <nttthoi@gmail.com>
2019-03-17 12:09:41 +0700

    change database

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+# class Clients(models.Model):
+#     email=models.EmailField(max_length=200,unique=True) 
+#     username=models.CharField(max_length=200,unique=True)
+#     password=models.CharField(max_length=200) 
+#     def __str__(self):
+#         return self.name
+
+#     class Meta:
+#         db_table = u'Clients'
--- officematter/models.py
+++ officematter/models.py
@@ -22,7 +22,24 @@ class Clients(models.Model):
     username=models.CharField(max_length=200,unique=True)
     password=models.CharField(max_length=200) 
     def __str__(self):
-        return self.name
+        return self.username
 
     class Meta:
         db_table = u'Clients'
+class ClientsInformation(models.Model):
+    Teacher = 'teach'
+    Student = 'stu' 
+    ROLES_CHOICES = (
+        (Teacher, 'Teacher'),
+        (Student, 'Student')     
+    )
+    user=models.ForeignKey(Clients,on_delete=models.PROTECT)
+    roles=models.CharField(
+        max_length=2,
+        choices=ROLES_CHOICES,
+        default=Student
+    )
+    class Meta:
+        db_table = u'ClientsInformation'
+
+
