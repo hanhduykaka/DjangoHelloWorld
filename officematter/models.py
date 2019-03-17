e4ebe24da5ed3a9a756016b29f0f3e890e3db53e
nttthoi <nttthoi@gmail.com>
2019-03-17 10:08:12 +0700

    modified views add fixed form && add cookie syntax simple

--- officematter/models.py
+++ officematter/models.py
@@ -20,7 +20,7 @@ class WebPage(models.Model):
 class Clients(models.Model):
     email=models.EmailField(max_length=200,unique=True) 
     username=models.CharField(max_length=200,unique=True)
-    password=models.CharField(max_length=200,unique=True) 
+    password=models.CharField(max_length=200) 
     def __str__(self):
         return self.name
 
--- officematter/views.py
+++ officematter/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from officematter.models import Topic, WebPage
 from officematter.form import FormRegister
 from officematter.models import Clients
+from datetime import datetime
 # Create your views here.
 
 
@@ -10,7 +11,10 @@ def index(request):
 
 
 def about(request):
-    return render(request, "officematter/about.html")
+    userCookie=''
+    if '__django__.User' in request.COOKIES:
+        userCookie = request.COOKIES['__django__.User']
+    return render(request, "officematter/about.html",context={"userCookie":userCookie})
 
 
 def topicPage(request):
@@ -23,18 +27,22 @@ def Web_Page(request):
 
 def register_view(request):  
     form = FormRegister()
+    create_date=datetime.now()
+    userName=''
     if request.method=='POST':
         form=FormRegister(request.POST,Clients)
         if form.is_valid() and form.cleaned_data['password'] == form.cleaned_data['confirm']:
             request.POST._mutable = True
             post = form.save(commit=False)
             post.email=form.cleaned_data['email']
-            post.username = form.cleaned_data['username']
+            userName=form.cleaned_data['username']
+            post.username = userName
             post.password = form.cleaned_data['password']
-            post.save()
+            post.save() 
             print("đã insert dư liệu")
-        # elif form.cleaned_data['password'] != form.cleaned_data['confirm']:
-        #     form.add_error('confirm','the password do not match')
-        #     print('pass word vs confirm password are not the same')
-
-    return render(request, "officematter/register.html",{'form':form})
+        else:
+            form.add_error('confirm','form không hợp lệ')
+            print('form không hợp lệ')
+    response= render(request, "officematter/register.html",{'form':form})
+    response.set_cookie('__django__.User',{"username":userName,"create_date":create_date.strftime('%d-%m-%Y %H:%M:%S')})
+    return response
